5a4adc2056876c8554310c15ed003d5bca1cda69
hyc <hthousandflame@gmail.com>
2024-02-14 00:01:45 +0800

    [add]drives makefile

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,3 +1,9 @@
 //
 // Created by hyc on 24-2-4.
 //
+#include <cstdio>
+
+int main()
+{
+    printf("hello world\n");
+}
---
+++ drives/inc/motor.h
@@ -0,0 +1,6 @@
+#ifndef _MOTOR_H
+#define _MOTOR_H
+
+#define ABC_XXX 1
+
+#endif
