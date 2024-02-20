1d846f24ece198ccdc82f99cbca6624b67077460
hyc <hthousandflame@gmail.com>
2024-02-20 22:11:33 +0800

    [feat] drive compilation framework

--- drives/inc/motor.h
+++ drives/inc/motor.h
@@ -1,6 +1,12 @@
 #ifndef _MOTOR_H
 #define _MOTOR_H
 
-#define ABC_XXX 1
+#include <stdint.h>
+struct motor_struct {
+    uint16_t speed;
+    uint8_t direction;
+    uint16_t max;
+    uint16_t min;
+};
 
 #endif
--- drives/src/motor.c
+++ drives/src/motor.c
@@ -19,62 +19,104 @@
 #include <linux/io.h>
 #include <linux/uio.h>
 #include <linux/uaccess.h>
+#include <linux/platform_device.h>
+#include <linux/of_device.h>
 
-#include "../inc/motor.h"
+#define DEVICE_NAME "yc_motor"
 
-static int major;
-
-static int hello_open (struct inode *node, struct file *filp)
-{
-    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-    return 0;
-}
-static ssize_t hello_read (struct file *filp, char __user *buf, size_t size, loff_t *offset)
-{
-    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-    return size;
-}
-
-static ssize_t hello_write(struct file *filp, const char __user *buf, size_t size, loff_t *offset)
-{
-    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-    return size;
-}
-
-static int hello_release (struct inode *node, struct file *filp)
-{
-    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
-    return 0;
-}
-
-/* 1. create file_operations */
-static const struct file_operations hello_drv = {
-    .owner      = THIS_MODULE,
-	.read		= hello_read,
-	.write		= hello_write,
-	.open		= hello_open,
-    .release    = hello_release,
+struct motor_dev_struct {
+    int pwm;
+    int speed;
 };
 
+static int major = 0;
+static struct class *motor_class;
+struct motor_dev_struct *motor_dev;
 
-/* 2. register_chrdev */
-
-/* 3. entry function */
-static int hello_init(void)
+static int motor_open(struct inode *node, struct file *filp)
 {
-    printk("hello %d", ABC_XXX);
-    major = register_chrdev(0, "100ask_hello", &hello_drv);
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    return 0;
+}
+static ssize_t motor_read(struct file *filp, char __user *buf, size_t size, loff_t *offset)
+{
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    return size;
+}
+
+static ssize_t motor_write(struct file *filp, const char __user *buf, size_t size, loff_t *offset)
+{
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
+    return size;
+}
+
+static int motor_release(struct inode *node, struct file *filp)
+{
+    printk("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__);
     return 0;
 }
 
+static const struct file_operations motor_drv = {
+    .owner = THIS_MODULE,
+    .read = motor_read,
+    .write = motor_write,
+    .open = motor_open,
+    .release = motor_release,
+};
 
-/* 4. exit function */
-static void hello_exit(void)
+static int motor_probe(struct platform_device *pdev)
 {
-    unregister_chrdev(major, "100ask_hello");
+
+    // todo get pwms
+    // todo multi device
+
+    major = register_chrdev(0, DEVICE_NAME, &motor_drv); /* /dev/led */
+
+    motor_class = class_create(THIS_MODULE, "motor_class");
+    if (IS_ERR(motor_class)) {
+        return PTR_ERR(motor_class);
+    }
+
+    device_create(motor_class, NULL, MKDEV(major, 0), NULL, DEVICE_NAME "%d", 0); /* /dev/100ask_led0 */
+
+    return 0;
+}
+
+static int motor_remove(struct platform_device *pdev)
+{
+    device_destroy(motor_class, MKDEV(major, 0));
+    class_destroy(motor_class);
+    unregister_chrdev(major, DEVICE_NAME);
+
+    return 0;
+}
+
+static const struct of_device_id motor_id[] = {
+    {.compatible = "yc,motor"},
+    {},
+};
+
+static struct platform_driver chip_motor_driver = {
+    .probe = motor_probe,
+    .remove = motor_remove,
+    .driver = {
+        .name = "yc_motor",
+        .of_match_table = motor_id,
+    },
+};
+
+static int motor_init(void)
+{
+    return platform_driver_register(&chip_motor_driver);
 }
 
 
-module_init(hello_init);
-module_exit(hello_exit);
+static void motor_exit(void)
+{
+    platform_driver_unregister(&chip_motor_driver);
+}
+
+
+module_init(motor_init);
+module_exit(motor_exit);
 MODULE_LICENSE("GPL");
