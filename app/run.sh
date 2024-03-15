67420121121da3aa17a21206871311cd63d53ac8
hyc <huangyuancan@pointspread.cn>
2024-03-15 11:52:13 +0800

    [add] add sensor frame of app

--- app/run.sh
+++ app/run.sh
@@ -1,6 +1,7 @@
 TOOLCHAIN_DIR=/home/hyc/Project/orangepi-build/toolchains/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu
-CMAKE_CXX_COMPILER=${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++
-CMAKE_C_COMPILER=${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-gcc 
+CXX_COMPILER=${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-g++
+C_COMPILER=${TOOLCHAIN_DIR}/bin/aarch64-none-linux-gnu-gcc 
 
-cmake -GNinja -B build -S .
+rm -rf build/*
+cmake -GNinja -B build -S . -DCMAKE_CXX_COMPILER=${CXX_COMPILER} -DCMAKE_C_COMPILER=${C_COMPILER}
 ninja -C build
